10f7b0974ba3d7ea0444505f6f7bd7821525b93f
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-08-03 14:21:00 -0300

    chore: add error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,8 @@
 import 'reflect-metadata'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { userRouter } from './routes/users.routes'
 import { AppDataSource } from './data-source'
+import { AppError } from './AppError'
 
 AppDataSource.initialize()
 
@@ -10,5 +11,21 @@ const app = express()
 app.use(express.json())
 
 app.use(userRouter)
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+	if(err instanceof AppError) {
+		return response.status(err.statusCode).json({
+			status: 'error',
+			message: err.message,
+		})
+	}
+
+	console.log(err)
+
+	return response.status(500).json({
+		status: 'error',
+		message: 'Internal Server Error',
+	})
+})
 
 export { app }
