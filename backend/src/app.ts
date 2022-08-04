8f0998cb5dfe421a173a69654058c2070f4a3f82
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-08-04 00:08:00 -0300

    fix: create users error solved

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata'
+import 'express-async-errors'
 import express, { NextFunction, Request, Response } from 'express'
 import { userRouter } from './routes/users.routes'
 import { AppDataSource } from './data-source'
--- backend/src/users/controller/CreateUserController.ts
+++ backend/src/users/controller/CreateUserController.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 import { UsersRepository } from "../repository/UsersRepository";
+import { CreateUserService } from "../service/CreateUserService";
+
+const usersRepository = new UsersRepository()
 
 export class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, email, RA, CPF } = request.body
 
-    const repository = new UsersRepository()
+    const createUserService = new CreateUserService(usersRepository)
 
-    const user = await repository.create({
+    const user = await createUserService.execute({
       name,
       email,
       RA,
