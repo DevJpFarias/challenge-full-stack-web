c5a6a5c6314ba0eea759b847a888d57991178f0d
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-08-06 12:09:40 -0300

    test: create createUser endpoint

--- backend/src/modules/user/service/createUser/CreateUserService.spec.ts
+++ backend/src/modules/user/service/createUser/CreateUserService.spec.ts
@@ -1,13 +1,20 @@
+import { AppError } from "../../../../shared/errors/AppError"
+import { FakeStudentsRepository } from "../../../student/repositories/fakes/FakeStudentsRepository"
+import { CreateStudentService } from "../../../student/service/CreateStudent/CreateStudentService"
 import { FakeUsersRepository } from "../../repositories/fakes/FakeUsersRepository"
 import { CreateUserService } from "./CreateUserService"
 
 let fakeUsersRepository: FakeUsersRepository
+let fakeStudentsRepository: FakeStudentsRepository
 let createUserService: CreateUserService
+let createStudentsService: CreateStudentService
 
 describe('Create User Test', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository()
-    createUserService = new CreateUserService(fakeUsersRepository)
+    fakeStudentsRepository = new FakeStudentsRepository()
+    createUserService = new CreateUserService(fakeUsersRepository, fakeStudentsRepository)
+    createStudentsService = new CreateStudentService(fakeStudentsRepository)
   })
 
   it('Should be able to create a new user', async () => {
@@ -22,5 +29,42 @@ describe('Create User Test', () => {
     expect(user).toHaveProperty('id')
     expect(user).toHaveProperty('isAdmin', false)
     expect(user).toHaveProperty('inactivated', false)
+  })
+
+  it('Should not be able to create an user with an existent email', async () => {
+    expect(async () => {
+      const data = {
+        name: 'Fulano',
+        email: 'fulano@mail.com',
+        password: '12345'
+      }
+      const second_data = {
+        name: 'Fulano',
+        email: 'fulano@mail.com',
+        password: '12345'
+      }
+  
+      await createUserService.execute(data)
+      await createUserService.execute(second_data)
+    }).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('Should not be able to create an user with an existent email (student)', () => {
+    expect(async () => {
+      const student_data = {
+        name: 'João Paulo',
+        email: 'joaopaulo@mail.com',
+        RA: 12345,
+        CPF: '999.999.999-99'
+      }
+      const user_data = {
+        name: 'Fulano',
+        email: 'joaopaulo@mail.com',
+        password: '12345'
+      }
+  
+      await createStudentsService.execute(student_data)
+      await createUserService.execute(user_data)
+    }).rejects.toBeInstanceOf(AppError)
   })
 })
