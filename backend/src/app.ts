f1c6fba522fdebefb7d160b68f8b2a59d21fcd5e
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-08-09 15:04:41 -0300

    docs: create documentation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata'
 import 'express-async-errors'
 import express, { NextFunction, Request, Response } from 'express'
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from './swagger.json'
 import { AppDataSource } from './shared/connections/data-source'
 import { AppError } from './shared/errors/AppError'
 import { routes } from './shared/routes'
@@ -12,6 +14,8 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 	if(err instanceof AppError) {
